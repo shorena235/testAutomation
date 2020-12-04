0ad4be9fc10e3ea8113cfad8f3cc113e17934536
Shorena Tsikaradze <shorenatsikaradze11@gmail.com>
2020-12-04 16:03:05 -0500

    initial commit

--- src/main/java/Myclass.java
+++ src/main/java/Myclass.java
@@ -1,10 +1,12 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-public class Myclass {
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+class testetsng {
 
     public String basePath = ("http://the-internet.herokuapp.com/");
     public WebDriver driver;
